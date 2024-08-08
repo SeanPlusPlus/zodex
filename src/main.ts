f36fd49622301bd358dfcf859b40a4f5c19a9b71
Sean Stephenson <sean.m.stephenson@disney.com>
2024-08-08 12:16:24 -0700

    ZOD_TS_EXTENDED_API: no if

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,7 @@ type User = z.infer<typeof userSchema>
 async function fetchEntries() {
   try {
     const response = await fetch('https://simplejson.vercel.app/api/QQtkYrO9') 
-    if (!response.ok) {
-      throw new Error('Network response was not ok')
-    }
+    
     const data: User[] = await response.json()
 
     data.forEach((entry) => {
